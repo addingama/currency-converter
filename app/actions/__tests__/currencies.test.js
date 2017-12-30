f72e972a40de09ff5cb865f516083de2ee806289
Addin Gama Bertaqwa <punyagama@gmail.com>
2017-12-30 17:57:13 +0800

    Test redux actions

---
+++ app/actions/__tests__/currencies.test.js
@@ -0,0 +1,14 @@
+import {
+  getInitialConversion,
+  GET_INITIAL_CONVERSION,
+  changeCurrencyAmount,
+  CHANGE_CURRENCY_AMOUNT,
+} from '../currencies';
+
+it('create a GET_INITIAL_CONVERSION action', () => {
+  expect(getInitialConversion()).toMatchSnapshot();
+});
+
+it('create a CHANGE_CURRENCY_AMOUNT action', () => {
+  expect(changeCurrencyAmount('1000')).toMatchSnapshot();
+});
